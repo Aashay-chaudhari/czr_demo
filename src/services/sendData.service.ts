738f3d98afff288a06be67985a04abe5f8d0197a
Aashay Chaudhari <93089131+Aashay-chaudhari@users.noreply.github.com>
2023-06-29 17:52:07 -0400

    Update sendData.service.ts

--- src/services/sendData.service.ts
+++ src/services/sendData.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class sendDataService {
-  token = 'VdSqsVk7JBgPJgbckjLD8WuI2TqT99e3QRkOxNg6'
+  token = 'Enter token'
   constructor(private http: HttpClient) { }
   public sendInfo(params:any){
 
@@ -13,7 +13,7 @@ export class sendDataService {
         const headers = new HttpHeaders({
           'X-Api-Key': this.token
         });
-       apiUrl = 'https://5fm7cr8leb.execute-api.us-east-1.amazonaws.com/dev/send';
+       apiUrl = 'Enter API gateway endpoint';
 
     return this.http.post(apiUrl,params,{ headers: headers });
   }
